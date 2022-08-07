110ca3246d8cb67e5e0a633a5e46b0ae2fb9f406
muntashir-wahid <muntashir.wahid@yahoo.com>
2022-08-07 15:07:32 +0600

    started to use redux data in react component

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+// const redux = require("redux");
+import { legacy_createStore } from "redux";
+
+const counterReducer = function (state = { counter: 0 }, action) {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+
+  return state;
+};
+
+const store = legacy_createStore(counterReducer);
+
+export default store;
